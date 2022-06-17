3824cb06e0582d20b50257533b2e9ba2b7498476
Anderson Colin <andercol@gmail.com>
2022-06-17 15:35:08 -0300

    add compontes ItemMenu, MenuTree e Footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="https://github.com/andercol" target='_blank'> Anderson Colin</a>
+        </strong>
+    </footer>
+)

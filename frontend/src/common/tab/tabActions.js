4d293d441ac7d3fdeca1b2bbe1b6ec57d4a81b70
Anderson Colin <andercol@gmail.com>
2022-07-01 07:31:16 -0300

    implementado Action de visibilidade das abas

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -1,6 +1,19 @@
-export function selectTab(tabId){
-    return{
+export function selectTab(tabId) {
+    return {
         type: 'TAB_SELECTED',
         payload: tabId
+    }
+}
+
+/* exemplo de chamada
+    showTabs('tabList', 'tabCreate') 
+*/
+
+export function showTabs(...tabIds) {
+    const tabsToShow = {}
+    tabIds.forEach(e => tabsToShow[e] = true)
+    return{
+        type:'TAB_SHOW',
+        payload: tabsToShow
     }
 }
--- frontend/src/common/tab/tabContent.jsx
+++ frontend/src/common/tab/tabContent.jsx
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import If from '../operador/if'
+
 class TabContent extends Component {
+
     render() {
         const selected = this.props.tab.selected === this.props.id
+        const visible = this.props.tab.visible[this.props.id]
+        
+        console.log('tabContent Visible: ', this.props.id, ' ', visible)
+        /* 
+            this.props.tab.visible -> vem do state mapeado (tabReducer)
+            this.props.id -> vem das propriedades
+         */
         return (
-            <div id={this.props.id}
-                className={`tab-pane ${selected ? 'active' : ''}`}>
-                {this.props.children}
-            </div>
+            <If test={visible}>
+                <div id={this.props.id}
+                    className={`tab-pane ${selected ? 'active' : ''}`}>
+                    {this.props.children}
+                </div>
+            </If>
         )
     }
 }
